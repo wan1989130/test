8ac927bffc38f60060eb519b3cf24031979b5638
王岩 <315259756@qq.com>
2018-12-29 13:34:45 +0800

    提交

---
+++ 递归/递归/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  递归
+//
+//  Created by 王岩 on 2017/8/11.
+//  Copyright © 2017年 MR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var flag = true
+    var index = 0
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        for i in 0..<5 {
+            
+            if i == 1{
+                flag = false
+            }
+            test()
+        }
+        test2()
+    }
+
+    func test(){
+        index = index + 1
+        if flag {
+            print("a")
+        }else{
+            if index <= 5{
+                print("b")
+                test()
+            }
+        }
+    }
+    func test2(){
+        print("c")
+    }
+}
+
